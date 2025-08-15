cbf84c62f4feddd63d507a8e23ecbc9dad9e5418
welljhon <jhon-437699@outlook.com>
2025-08-15 17:48:49 -0400

    add auto scroll to recipe new message

--- src/components/chat/chatMessage.vue
+++ src/components/chat/chatMessage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="msgRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <ChatBouble v-for="message in messages" :key="message.Id" v-bind="message" />
@@ -10,10 +10,25 @@
 <script setup lang="ts">
 import type { ChatMessage } from '@/interfaces/chat-message.interface'
 import ChatBouble from './chatBouble.vue'
+import { ref, watch, nextTick } from 'vue'
 
 interface Props {
   messages: ChatMessage[]
 }
 
-defineProps<Props>()
+const { messages } = defineProps<Props>()
+
+const msgRef = ref<HTMLDivElement | null>(null)
+
+watch(
+  () => messages,
+  async () => {
+    await nextTick()
+    msgRef.value?.scrollTo({
+      top: msgRef.value.scrollHeight,
+      behavior: 'smooth',
+    })
+  },
+  { deep: true },
+)
 </script>
